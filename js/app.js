c23849b279a97ad0bea6b03e7f57cd0b27361e26
elisa33 <pipi.tdfuego@gmail.com>
2021-01-19 11:43:24 -0400

    delete button

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ class Citas {
 	}
 	agregarCita(cita) {
 		this.citas = [...this.citas, cita];
-		console.log(this.citas);
+	}
+	eliminarCita(id) {
+		this.citas = this.citas.filter((cita) => cita.id !== cita.id);
 	}
 }
 
@@ -41,7 +43,6 @@ class UI {
 	}
 	imprimirCitas({ citas }) {
 		this.limpiarHTML();
-
 		citas.forEach((cita) => {
 			const {
 				mascota,
@@ -76,12 +77,28 @@ class UI {
 			const sintomasParrafo = document.createElement('p');
 			sintomasParrafo.innerHTML = `<span class='font-weight-bolder'>Sintomas </span>${sintomas}`;
 
+			// boton eliminar
+			const btnEliminar = document.createElement('button');
+			btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+			btnEliminar.innerHTML = `Eliminar <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`;
+
+			btnEliminar.onclick = () => eliminarCita(id);
+
+			// Añade boton para editar
+			const btnEditar = document.createElement('button');
+			btnEditar.classList.add('btn', 'btn-info');
+			btnEditar.innerHTML = `Editar <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>`;
+
+			btnEditar.onclick = () => cargarEdicion(cita);
+
 			divCita.appendChild(mascotaParrafo);
 			divCita.appendChild(propietarioParrafo);
 			divCita.appendChild(telefonoParrafo);
 			divCita.appendChild(fechaParrafo);
 			divCita.appendChild(horaParrafo);
 			divCita.appendChild(sintomasParrafo);
+			divCita.appendChild(btnEliminar);
+			divCita.appendChild(btnEditar);
 			contenedorCitas.appendChild(divCita);
 		});
 	}
@@ -158,4 +175,12 @@ function reiniciarObjeto() {
 	citaObj.fecha = '';
 	citaObj.hora = '';
 	citaObj.sintomas = '';
+}
+function eliminarCita(id) {
+	// eliminar la cita
+	administrarCitas.eliminarCita(id);
+	// mensaje
+	ui.imprimirAlerta('La cita se elimino correctamente');
+	//refresque
+	ui.imprimirCitas(administrarCitas);
 }
